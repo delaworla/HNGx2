b8bd20f9a8347c288ce534e5dd113ecf5fcf089b
delaworla <worlanuel@hotmail.com>
2023-09-05 10:25:18 +0000

    refactor

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def create_person(person: Person):
 
 
 @app.get("/api/person/{person_id}")
-async def read_person(person_id: int):
-    if person_id not in my_people:
+async def read_person(id: int):
+    if id not in my_people:
         raise HTTPException(status_code=404, detail="Person not found")
-    return db[person_id]
+    return {"data": f"Here is person {'id'}"}
