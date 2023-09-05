8cf05431a88693a06150ad1ddbe80cd47f52baef
delaworla <worlanuel@hotmail.com>
2023-09-05 09:49:31 +0000

    appending it to the dictionary

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def get_persons():
 async def create_person(person: Person):
     person_dict = person.dict()
     person_dict['id'] = randrange(0,10000000)
-    
+    my_people.append()
     return person
 
 
