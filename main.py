d64cb972f55f5bf6d907be5caa86d98aeea8aa27
delaworla <worlanuel@hotmail.com>
2023-09-05 09:46:16 +0000

    converting to dictionary

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from random import randrange
 
 app = FastAPI()
 
+
 class Person(BaseModel):
     name: str
-    age: str 
+    age: str
     email: str
 
-my_people = [{"name": "Emma","age": "23","email": "emma@hngx.com", "id":1}, {"name": "Sam","age": "23","email": "sam@hngx.com", "id":2} ]
+
+my_people = [{"name": "Emma", "age": "23", "email": "emma@hngx.com",
+              "id": 1}, {"name": "Sam", "age": "23", "email": "sam@hngx.com", "id": 2}]
 
 
 @app.get("/api/persons")
@@ -20,15 +23,15 @@ def get_persons():
 
 @app.post("/api/person/", response_model=Person)
 async def create_person(person: Person):
+    person_dict = person.dict()
+    
     person_id = len(my_people) + 1
     my_people[person_id] = person.dict()
     return person
 
-    
+
 @app.get("/api/person/{person_id}", response_model=Person)
 async def read_person(person_id: int):
     if person_id not in db:
         raise HTTPException(status_code=404, detail="Person not found")
     return db[person_id]
-
-    
