23e40fbbaf05014280e50a47d94826c88333ff26
delaworla <worlanuel@hotmail.com>
2023-09-05 10:07:23 +0000

    making it async

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_persons():
 
 
 @app.post("/api/person/")
-def create_person(person: Person):
+async def create_person(person: Person):
     person_dict = person.dict()
     person_dict['id'] = randrange(0, 10000000)
     my_people.append(person_dict)
