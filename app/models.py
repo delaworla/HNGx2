efc7d25c3b793590bed7e8de3a03179eee5bb920
delaworla <worlanuel@hotmail.com>
2023-09-12 10:20:43 +0000

    fields

--- app/models.py
+++ app/models.py
@@ -4,4 +4,5 @@ from sqlalchemy import Column, Integer, String
 
 class Persons(Base):
     __tabelename__ ="persons"
-    
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
