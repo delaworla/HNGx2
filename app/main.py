a00d5bf3c13bc6511935e684bea5d506db11cde2
delaworla <worlanuel@hotmail.com>
2023-09-13 14:19:02 +0000

    return values

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ def get_persons():
 
 @app.post("/api/person/", status_code=status.HTTP_201_CREATED)
 async def create_person(person: Person):
-    cursor.execute(""" INSERT INTO people(name,age) VALUES(%s,%s)""", (person.name, person.age))
+    cursor.execute(""" INSERT INTO people(name,age) VALUES(%s,%s) RETURNING *""", (person.name, person.age))
     new_people =cursor.fetchone()
     return {"data": "new_people"}
 
