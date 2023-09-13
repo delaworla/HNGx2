289bc2ae3a38d1f17176622bdae0c17791dc534c
delaworla <worlanuel@hotmail.com>
2023-09-13 13:22:23 +0000

    connected successfully to db

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel
 from typing import Dict
 from random import randrange
 from starlette import status
-import requests
-import psycopg2
+import requests, psycopg2
+
 from psycopg2.extras import RealDictConnection
 
 app = FastAPI()
@@ -15,16 +15,19 @@ class Person(BaseModel):
     age: str
     email: str
 
+
 try:
-    conn = psycopg2.connect(host='localhost', database='HNGx', user='postgres', password='postgres', cursor_factory=RealDictConnection)
+    conn = psycopg2.connect(host='localhost', database='HNGx', user='postgres',
+                            password='postgres', cursor_factory=RealDictConnection)
     cursor = conn.cursor
     print("Database was connected successfully")
 except Exception as error:
-    print ("Connection to database failed")
-    print ("Error:", error)
+    print("Connection to database failed")
+    print("Error:", error)
 
 my_people = [{"name": "Emma", "age": "23", "email": "emma@hngx.com",
               "id": 1}, {"name": "Sam", "age": "23", "email": "sam@hngx.com", "id": 2}]
+
 
 @app.get("/api/persons")
 def get_persons():
@@ -60,19 +63,22 @@ def find_index_person(id):
             return i
 
 
-@app.delete("/api/person/{id}", status_code = status.HTTP_204_NO_CONTENT)
+@app.delete("/api/person/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_person(id: int):
     index = find_index_person(id)
     if index == None:
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail =f"post with id: {id} does not exist")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist")
     my_people.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
 
 @app.put("/api/person/{id}")
 async def update_person(id: int, person: Person):
     index = find_index_person(id)
     if index == None:
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail =f"post with id: {id} does not exist")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist")
     person_dict = person.dict()
     person_dict['id'] = id
     my_people[index] = person_dict
