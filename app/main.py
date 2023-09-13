7e6ee9f3f06534986d5a278267355ed2809f236c
delaworla <worlanuel@hotmail.com>
2023-09-13 11:26:49 +0000

    mapping it

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ class Person(BaseModel):
 
 try:
     conn = psycopg2.connect(host='localhost', database='HNGx', user='postgres', password='postgres', cursor_factory=RealDictConnection)
+    cursor = conn.cursor
 
 my_people = [{"name": "Emma", "age": "23", "email": "emma@hngx.com",
               "id": 1}, {"name": "Sam", "age": "23", "email": "sam@hngx.com", "id": 2}]
