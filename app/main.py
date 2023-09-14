d9d2045f55f8fe5bf7fbb1f39c1ecf9accd25ed4
delaworla <worlanuel@hotmail.com>
2023-09-14 11:03:47 +0000

    cleaning code

--- app/main.py
+++ app/main.py
@@ -16,13 +16,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+
 
 class Person(BaseModel):
     name: str
