b37e44e7e30f768c8cd291e9b90dc840c35d3d61
delaworla <worlanuel@hotmail.com>
2023-09-14 10:45:35 +0000

    making it postgresql

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = 'postgres://postgres:postgres@localhost/persons'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost/persons'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
