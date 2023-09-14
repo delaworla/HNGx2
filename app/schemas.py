0cfdcf5f7834ed2b4cc3bb4941267e1980d8d771
delaworla <worlanuel@hotmail.com>
2023-09-14 15:51:15 +0000

    schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,2 +1,15 @@
 from pydantic import BaseModel
 
+
+class Person(BaseModel):
+    name: str
+    age: str
+    
+
+class CreatePerson(BaseModel):
+    name: str
+    age: str
+    
+class UpdatePerson(BaseModel):
+    name: str
+    age: str
