aed139db490a7912dce470a81fb0fbdb37f14979
delaworla <worlanuel@hotmail.com>
2023-09-15 15:21:32 +0000

    importing pytz

--- app/models.py
+++ app/models.py
@@ -8,5 +8,4 @@ class Persons(Base):
     __tablename__ ="persons"
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String, nullable=False)
-    
     created_at=Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel
+from datetime import datetime
+from pytz import timezone
 
 
 class Person(BaseModel):
@@ -13,6 +15,7 @@ class CreatePerson(BaseModel):
 class UpdatePerson(BaseModel):
     name: str
     
-
+current_time_utc = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
 class Response(BaseModel):
-    
+    name: str
+    created_at: current_time_utc
