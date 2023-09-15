dbbb8f758421d62158aeb4004609f4d4cf444af2
delaworla <worlanuel@hotmail.com>
2023-09-15 13:17:42 +0000

    removing age from schemas

--- app/schemas.py
+++ app/schemas.py
@@ -3,13 +3,12 @@ from pydantic import BaseModel
 
 class Person(BaseModel):
     name: str
-    age: str
+
     
 
 class CreatePerson(BaseModel):
     name: str
-    age: str
+
     
 class UpdatePerson(BaseModel):
     name: str
-    age: str
