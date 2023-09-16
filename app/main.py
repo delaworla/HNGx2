3853441dcd786aa67fafa890d5fc6a1f740614b4
delaworla <worlanuel@hotmail.com>
2023-09-16 11:06:55 +0000

    api

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 
 
-@app.get("/")
+@app.get("/api")
 def get_persons(db:Session =Depends(get_db)):
     persons = db.query(models.Persons).order_by(asc(models.Persons.id)).all()
     return persons
