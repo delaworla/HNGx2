8a59fdab454927d3350884c3d9b5f3a5a7c04096
delaworla <worlanuel@hotmail.com>
2023-09-18 14:57:15 +0000

    models

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from .database import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
+
+class Persons(Base):
+    __tablename__ ="persons"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    created_at=Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    
