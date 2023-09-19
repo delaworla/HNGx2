1acbea66f3cd8ba7094a102205ee35041911a7f2
delaworla <worlanuel@hotmail.com>
2023-09-19 16:55:17 +0000

    columns for users

--- app/models.py
+++ app/models.py
@@ -13,5 +13,8 @@ class Persons(Base):
     
 class Users(Base):
     __tablename__ ="users"
-    
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    created_at=Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     
