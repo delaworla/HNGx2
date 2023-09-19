5d9f34b088bf0c0d4ed51b64f33a8af4975ff2a3
delaworla <worlanuel@hotmail.com>
2023-09-19 15:40:59 +0000

    clearing update

--- app/schemas.py
+++ app/schemas.py
@@ -4,9 +4,7 @@ from pydantic import BaseModel
 class Person(BaseModel):
     name: str
 
-class Update(BaseModel):
-    name: str 
-    last_modified: datetime
+
 class Response(BaseModel):
     id: int
     name: str 
